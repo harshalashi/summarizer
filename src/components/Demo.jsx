6233e9de65f7d0111c18eab3ba3ad43e4c777b6f
Harsh <imharshalashi333@gmail.com>
2023-05-31 22:10:31 +0530

    input field created

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+
+import { copy, linkIcon, loader, tick } from '../assets';
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const handleSubmit = async (e) => {
+    //API request
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        {/* Input URL Form */}
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ✔️
+          </button>
+        </form>
+
+        {/* Browse URL History */}
+      </div>
+      {/* Display Results */}
+    </section>
+  );
 };
 
 export default Demo;
