19c06db79fb6c379ce711b242bc2d71d8848d45f
Harsh <imharshalashi333@gmail.com>
2023-05-31 21:40:07 +0530

    Hero Section Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,14 @@ const Hero = () => {
           Github
         </button>
       </nav>
+      <h1 className="head_text">
+        Summarize Article with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your readings with Sumz, an open-source article summarizer that
+        transforms lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   );
 };
